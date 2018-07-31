bd4e4da0e70678487ca13a4f0ed030c35fa597e4
petercolella <peter.joseph.colella@gmail.com>
2018-07-30 23:29:46 -0400

    Lecture 223

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -45,11 +46,11 @@ class ContactData extends Component {
 
     render () {
         let form = (
-            <form className={classes.Input}>
-                <input type="text" name="name" placeholder="Your Name" autoComplete="name" />
-                <input type="email" name="email" placeholder="Your Email" autoComplete="email" />
-                <input type="text" name="street" placeholder="Street" autoComplete="address-line1" />
-                <input type="text" name="zip" placeholder="Zip Code" autoComplete="postal-code" />
+            <form>
+                <Input inputtype="input" type="text" name="name" placeholder="Your Name" autoComplete="name" />
+                <Input inputtype="input" type="email" name="email" placeholder="Your Email" autoComplete="email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" autoComplete="address-line1" />
+                <Input inputtype="input" type="text" name="zip" placeholder="Zip Code" autoComplete="postal-code" />
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
