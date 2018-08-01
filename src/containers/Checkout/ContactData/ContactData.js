8df42128cebddf61192889ee4c0462417d3dfc09
petercolella <peter.joseph.colella@gmail.com>
2018-08-01 18:09:07 -0400

    Lecture 227

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,18 @@ class ContactData extends Component {
             } );
     }
 
+    eventChangedHandler = ( event, inputIdentifier ) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[ inputIdentifier ]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[ inputIdentifier ] = updatedFormElement;
+        this.setState( { orderForm: updatedOrderForm } );
+    }
+
     render () {
         const formElementsArray = [];
         for ( let key in this.state.orderForm ) {
@@ -102,7 +114,8 @@ class ContactData extends Component {
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value} />
+                        value={formElement.config.value}
+                        changed={( event ) => this.eventChangedHandler( event, formElement.id )} />
                 ) )}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
