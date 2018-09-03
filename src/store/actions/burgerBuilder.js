605e9bafa7053053d655c3a366b214bb2302bb08
petercolella <peter.joseph.colella@gmail.com>
2018-09-03 01:09:38 -0400

    Lecture 288.1

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -3,13 +3,13 @@ import * as actionTypes from './actionTypes';
 export const addIngredient = ( name ) => {
     return {
         type: actionTypes.ADD_INGREDIENT,
-        ingedientName: name
+        ingredientName: name
     };
 };
 
 export const removeIngredient = ( name ) => {
     return {
         type: actionTypes.REMOVE_INGREDIENT,
-        ingedientName: name
+        ingredientName: name
     };
 };
