e9aa4a7a675272975c0ae9d8bf86127dfa3f872b
petercolella <peter.joseph.colella@gmail.com>
2018-09-12 22:18:33 -0400

    Lecture 312

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,22 +8,36 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = ( authData ) => {
+export const authSuccess = (authData) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
         authData: authData
     };
 };
 
-export const authFail = ( error ) => {
+export const authFail = (error) => {
     return {
         type: actionTypes.AUTH_FAIL,
         error: error
     };
 };
 
-export const auth = ( email, password ) => {
+export const auth = (email, password) => {
     return dispatch => {
-        dispatch( authStart() );
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDwOe3O4407mjkV87E98sw-tASX0z7rZiU', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };
