afc58037391bfecc33d181ac18ffc0afc870e3a9
petercolella <peter.joseph.colella@gmail.com>
2018-09-17 00:11:32 -0400

    Lecture 320.1

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,46 +20,50 @@ class BurgerBuilder extends Component {
         purchasing: false
     }
 
-    componentDidMount () {
-        console.log( this.props );
+    componentDidMount() {
+        console.log(this.props);
         this.props.onInitIngredients();
     }
 
-    updatePurchaseState ( ingredients ) {
-        const sum = Object.keys( ingredients )
-            .map( igKey => {
-                return ingredients[ igKey ];
-            } )
-            .reduce( ( sum, el ) => {
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
                 return sum + el;
-            }, 0 );
+            }, 0);
         return sum > 0;
     }
 
     purchaseHandler = () => {
-        this.setState( { purchasing: true } );
+        if (this.props.isAuthenticated) {
+            this.setState({ purchasing: true });
+        } else {
+            this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
-        this.setState( { purchasing: false } );
+        this.setState({ purchasing: false });
     }
 
     purchaseContinueHandler = () => {
         this.props.onInitPurchase();
-        this.props.history.push( '/checkout' );
+        this.props.history.push('/checkout');
     }
 
-    render () {
+    render() {
         const disabledInfo = {
             ...this.props.ings
         };
-        for ( let key in disabledInfo ) {
-            disabledInfo[ key ] = disabledInfo[ key ] <= 0
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
         }
         let orderSummary = null;
         let burger = this.props.error ? <p>Ingredients can't be loaded.</p> : <Spinner />
 
-        if ( this.props.ings ) {
+        if (this.props.ings) {
             burger = (
                 <Aux>
                     <Burger ingredients={this.props.ings} />
@@ -67,8 +71,9 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.updatePurchaseState( this.props.ings )}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}
                         price={this.props.price} />
                 </Aux>
             );
@@ -94,17 +99,18 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: ( ingName ) => dispatch( actions.addIngredient( ingName ) ),
-        onIngredientRemoved: ( ingName ) => dispatch( actions.removeIngredient( ingName ) ),
-        onInitIngredients: () => dispatch( actions.initIngredients() ),
-        onInitPurchase: () => dispatch( actions.purchaseInit() )
+        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(actions.initIngredients()),
+        onInitPurchase: () => dispatch(actions.purchaseInit())
     }
 }
 
-export default connect( mapStateToProps, mapDispatchToProps )( withErrorHandler( BurgerBuilder, axios ) );
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
