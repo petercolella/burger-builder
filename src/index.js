6002143285a831e6427e52f085fa3fb8607bc162
petercolella <peter.joseph.colella@gmail.com>
2018-10-21 23:33:46 -0400

    Lecture 411

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import createSagaMiddleware from "redux-saga";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import burgerBuilderReducer from "./store/reducers/burgerBuilder";
-import orderReducer from "./store/reducers/order";
-import authReducer from "./store/reducers/auth";
-import { watchAuth } from "./store/sagas";
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
+import { watchAuth, watchBurgerBuilder } from './store/sagas';
 
 const composeEnhancers =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : null || compose;
 
@@ -33,6 +33,7 @@ const store = createStore(
 );
 
 sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
 
 const app = (
   <Provider store={store}>
@@ -42,5 +43,5 @@ const app = (
   </Provider>
 );
 
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
